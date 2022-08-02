0e107212a71ac7aba95b53364198c613cedf7ae5
Adrian Alvird <51330833+adrianalvird@users.noreply.github.com>
2022-08-02 12:05:40 +0530

    Add files via upload

--- eLearn/settings.py
+++ eLearn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-36_4hkz7+2mf0!zq^qlc16_txx()*ureu-&daymn=h^*8f36lt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['elearninstitution.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 MEDIA_URL = '/applications/'
